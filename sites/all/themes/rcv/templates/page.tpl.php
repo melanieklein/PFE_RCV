b9199719d088ad57551a5352d38c978006139441
Mélanie Klein <melaz_k@hotmail.fr>
2013-04-22 14:51:56 +0200

    Drupal
    Avancement dans le contenu et dans le theming

--- sites/all/themes/rcv/templates/page.tpl.php
+++ sites/all/themes/rcv/templates/page.tpl.php
@@ -74,50 +74,12 @@
 
   <header role="banner">
 
-    <?php if ($logo): ?>
-      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo"><img src="<?php print $logo; ?>" width="200" height="200" alt="<?php print t('Home'); ?>" /></a>
-    <?php endif; ?>
-
-    <?php if ($site_name || $site_slogan): ?>
-      <hgroup id="name-and-slogan">
         <?php if ($site_name): ?>
-          <h1 id="site-name">
-            <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-          </h1>
+          <h1 class="outliner"><?php print $site_name; ?></h1>
         <?php endif; ?>
 
-        <?php if ($site_slogan): ?>
-          <h2 id="site-slogan"><?php print $site_slogan; ?></h2>
-        <?php endif; ?>
-      </hgroup><!-- /#name-and-slogan -->
-    <?php endif; ?>
-        
-        <?php if ($main_menu): ?>
-        <nav class="menu-nav" role="navigation">
-        <h1 class="outliner">Menu de navigation</h1>
-          <?php
-          // This code snippet is hard to modify. We recommend turning off the
-          // "Main menu" on your sub-theme's settings form, deleting this PHP
-          // code block, and, instead, using the "Menu block" module.
-          // @see http://drupal.org/project/menu_block
-          print theme('links__system_main_menu', array(
-            'links' => $main_menu,
-            'attributes' => array(
-              'class' => array('links', 'inline', 'clearfix'),
-            ),
-            'heading' => array(
-              'text' => t('Main menu'),
-              'level' => 'h2',
-              'class' => array('element-invisible'),
-            ),
-          )); ?>
-        </nav>
-      <?php endif; ?>
-
-      <?php print render($page['navigation']); ?>
-
-    <?php if ($secondary_menu): ?>
-      <nav id="secondary-menu" role="navigation">
+        <?php if ($secondary_menu): ?>
+      <nav class="menu-login" role="navigation">
         <?php print theme('links__system_secondary_menu', array(
           'links' => $secondary_menu,
           'attributes' => array(
@@ -132,32 +94,53 @@
       </nav>
     <?php endif; ?>
 
+        <!--<?php print render($page['navigation']); ?>-->
+        
+        <?php if ($main_menu): ?>
+        <nav class="menu-nav" role="navigation">
+        <h1 class="outliner">Menu de navigation</h1>
+          <?php
+          // This code snippet is hard to modify. We recommend turning off the
+          // "Main menu" on your sub-theme's settings form, deleting this PHP
+          // code block, and, instead, using the "Menu block" module.
+          // @see http://drupal.org/project/menu_block
+          print theme('links__system_main_menu', array(
+            'links' => $main_menu,
+            'attributes' => array(
+              'class' => array('inline', 'clearfix'),
+            ),
+            'heading' => array(
+              'text' => t('Main menu'),
+              'level' => 'h2',
+              'class' => array('element-invisible'),
+            ),
+          )); ?>
+        </nav>
+      <?php endif; ?>
+
+    <?php if ($logo || $site_name): ?>
+      <a href="<?php print $front_page; ?>" title="<?php print $site_name; ?>" rel="home" id="logo"><img src="<?php print $logo; ?>" width="200" height="200" alt="<?php print $site_name; ?>" /></a>
+    <?php endif; ?>
+
     <?php print render($page['header']); ?>
 
   </header>
 
   <div class="content" role="main">
-
       <?php print render($page['highlighted']); ?>
       <?php /*print $breadcrumb;*/ ?>
       <a id="main-content"></a>
-      <?php print render($title_prefix); ?>
-      <?php if ($title): ?>
-        <h1 class="title" id="page-title"><?php print $title; ?></h1>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
       <?php print $messages; ?>
       <?php print render($tabs); ?>
-      <?php print render($page['help']); ?>
+      <!--<?php print render($page['help']); ?>-->
       <?php if ($action_links): ?>
         <ul class="action-links"><?php print render($action_links); ?></ul>
       <?php endif; ?>
       <?php print render($page['content']); ?>
       <?php print $feed_icons; ?>
-    </div><!-- /#content -->
+  </div><!-- /#content -->
   
   <footer>
-
   <?php print render($page['footer']); ?>
   </footer>
 
