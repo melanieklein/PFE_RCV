68ad8ef2ac72eda710d73ebbc127f07f1a194485
Mélanie Klein <melaz_k@hotmail.fr>
2013-05-01 22:06:45 +0200

    Tri des résultats de concours

---
+++ sites/all/themes/rcv/templates/page--node--4.tpl.php
@@ -0,0 +1,162 @@
+<?php
+/**
+ * @file
+ * Zen theme's implementation to display a single Drupal page.
+ *
+ * Available variables:
+ *
+ * General utility variables:
+ * - $base_path: The base URL path of the Drupal installation. At the very
+ *   least, this will always default to /.
+ * - $directory: The directory the template is located in, e.g. modules/system
+ *   or themes/bartik.
+ * - $is_front: TRUE if the current page is the front page.
+ * - $logged_in: TRUE if the user is registered and signed in.
+ * - $is_admin: TRUE if the user has permission to access administration pages.
+ *
+ * Site identity:
+ * - $front_page: The URL of the front page. Use this instead of $base_path,
+ *   when linking to the front page. This includes the language domain or
+ *   prefix.
+ * - $logo: The path to the logo image, as defined in theme configuration.
+ * - $site_name: The name of the site, empty when display has been disabled
+ *   in theme settings.
+ * - $site_slogan: The slogan of the site, empty when display has been disabled
+ *   in theme settings.
+ *
+ * Navigation:
+ * - $main_menu (array): An array containing the Main menu links for the
+ *   site, if they have been configured.
+ * - $secondary_menu (array): An array containing the Secondary menu links for
+ *   the site, if they have been configured.
+ * - $secondary_menu_heading: The title of the menu used by the secondary links.
+ * - $breadcrumb: The breadcrumb trail for the current page.
+ *
+ * Page content (in order of occurrence in the default page.tpl.php):
+ * - $title_prefix (array): An array containing additional output populated by
+ *   modules, intended to be displayed in front of the main title tag that
+ *   appears in the template.
+ * - $title: The page title, for use in the actual HTML content.
+ * - $title_suffix (array): An array containing additional output populated by
+ *   modules, intended to be displayed after the main title tag that appears in
+ *   the template.
+ * - $messages: HTML for status and error messages. Should be displayed
+ *   prominently.
+ * - $tabs (array): Tabs linking to any sub-pages beneath the current page
+ *   (e.g., the view and edit tabs when displaying a node).
+ * - $action_links (array): Actions local to the page, such as 'Add menu' on the
+ *   menu administration interface.
+ * - $feed_icons: A string of all feed icons for the current page.
+ * - $node: The node object, if there is an automatically-loaded node
+ *   associated with the page, and the node ID is the second argument
+ *   in the page's path (e.g. node/12345 and node/12345/revisions, but not
+ *   comment/reply/12345).
+ *
+ * Regions:
+ * - $page['header']: Items for the header region.
+ * - $page['navigation']: Items for the navigation region, below the main menu (if any).
+ * - $page['help']: Dynamic help text, mostly for admin pages.
+ * - $page['highlighted']: Items for the highlighted content region.
+ * - $page['content']: The main content of the current page.
+ * - $page['sidebar_first']: Items for the first sidebar.
+ * - $page['sidebar_second']: Items for the second sidebar.
+ * - $page['footer']: Items for the footer region.
+ * - $page['bottom']: Items to appear at the bottom of the page below the footer.
+ *
+ * @see template_preprocess()
+ * @see template_preprocess_page()
+ * @see zen_preprocess_page()
+ * @see template_process()
+ */
+?>
+
+<div class="page">
+
+  <header role="banner">
+
+        <?php if ($site_name): ?>
+          <h1 class="outliner"><?php print $site_name; ?></h1>
+        <?php endif; ?>
+
+        <?php if ($secondary_menu): ?>
+      <nav class="menu-login" role="navigation">
+        <?php print theme('links__system_secondary_menu', array(
+          'links' => $secondary_menu,
+          'attributes' => array(
+            'class' => array('links', 'inline', 'clearfix'),
+          ),
+          'heading' => array(
+            'text' => $secondary_menu_heading,
+            'level' => 'h2',
+            'class' => array('element-invisible'),
+          ),
+        )); ?>
+      </nav>
+    <?php endif; ?>
+
+        <!--<?php print render($page['navigation']); ?>-->
+        
+        <?php if ($main_menu): ?>
+        <nav class="menu-nav" role="navigation">
+        <h1 class="outliner">Menu de navigation</h1>
+          <?php
+          // This code snippet is hard to modify. We recommend turning off the
+          // "Main menu" on your sub-theme's settings form, deleting this PHP
+          // code block, and, instead, using the "Menu block" module.
+          // @see http://drupal.org/project/menu_block
+          print theme('links__system_main_menu', array(
+            'links' => $main_menu,
+            'attributes' => array(
+              'class' => array('inline', 'clearfix'),
+            ),
+            'heading' => array(
+              'text' => t('Main menu'),
+              'level' => 'h2',
+              'class' => array('element-invisible'),
+            ),
+          )); ?>
+        </nav>
+      <?php endif; ?>
+
+    <?php if ($logo || $site_name): ?>
+      <a href="<?php print $front_page; ?>" title="<?php print $site_name; ?>" rel="home" id="logo"><img src="<?php print $logo; ?>" width="200" height="200" alt="<?php print $site_name; ?>" /></a>
+    <?php endif; ?>
+
+    <?php print render($page['header']); ?>
+
+  </header>
+
+  <div class="content" role="main">
+      <?php print render($page['highlighted']); ?>
+      <?php /*print $breadcrumb;*/ ?>
+      <a id="main-content"></a>
+      <?php print $messages; ?>
+      <?php print render($tabs); ?>
+      <!--<?php print render($page['help']); ?>-->
+      <?php if ($action_links): ?>
+        <ul class="action-links"><?php print render($action_links); ?></ul>
+      <?php endif; ?>
+
+      <p><?php print render($page['content']['system_main']['nodes']['4']['body'][0]['#markup']);?></p>
+      <section class="section concours">
+        <h2>Résultats des concours</h2>
+        <div class="bloc concours-interclubs col3"><?php print render($page['content']['views_concours_interclubs-block']);?></div>
+        <div class="bloc concours-provincial col3"><?php print render($page['content']['views_concours_provincial-block']);?></div>
+        <div class="bloc concours-national col3"><?php print render($page['content']['views_concours_nationaux-block']);?></div>
+        <div class="bloc concours-etrangers col3"><?php print render($page['content']['views_concours_etrangers-block']);?></div>
+      </section>
+      <section class="section photos">
+        <?php print render($page['content']['views_photos_evenements-block']);?>
+      </section>
+
+
+      <?php print $feed_icons; ?>
+  </div><!-- /#content -->
+  
+  <footer>
+  <?php print render($page['footer']); ?>
+  </footer>
+
+</div><!-- /#page -->
+
+<?php print render($page['bottom']); ?>
